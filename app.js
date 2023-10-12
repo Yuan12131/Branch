df01d007200f618f7693809235f64083e0fb3c70
leeyuan <dbdks32123@gmail.com>
2023-10-12 11:38:30 +0900

    수정

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("hello")
+const http = require('http');
+
+const server = http.createServer((a, b) => {
+b.writeHead(200, {"Content-Type" : "text/plain"})
+b.write("모다")
+b.end();
+});
+
+server.listen(8019)
