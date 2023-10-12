a5ca8093141843093fcdde55f62b2ae5de9b85e7
leeyuan <dbdks32123@gmail.com>
2023-10-12 11:26:55 +0900

    향마루 -> 모다

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
 res.writeHead(200, {"Content-Type" : "text/plain"})
-res.write("향마루")
+res.write("모다")
 res.end();
 });
 
