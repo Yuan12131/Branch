6f72d7a273d4858127fb67c9ab315e212cc3131a
leeyuan <dbdks32123@gmail.com>
2023-10-12 11:31:21 +0900

    대폭 수정

--- app.js
+++ app.js
@@ -1,15 +1,1 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-res.writeHead(200, {"Content-Type" : "text/plain"})
-res.write("모다")
-res.end();
-});
-
-server.listen(3500, (err) => {
-  // if(!err){
-  //   console.error("서버 안돌아감")
-  // } else {
-  //   console.log("3500포트에 돌아감 http://localhost:3500")
-  // }
-})
+console.log("hello")
