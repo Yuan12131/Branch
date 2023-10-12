229c88f64ce7e2e84e881b15591183dad9bd6e78
leeyuan <dbdks32123@gmail.com>
2023-10-12 10:57:19 +0900

    향마루 응답처리

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-
+res.writeHead(200, {"Content-Type" : "text/plain"})
+res.write("향마루")
+res.end();
 });
 
 server.listen(3500, (err) => {
-  if(!err){
-    console.error("서버 안돌아감")
-  } else {
-    console.log("3500포트에 돌아감 http://localhost:3500")
-  }
+  // if(!err){
+  //   console.error("서버 안돌아감")
+  // } else {
+  //   console.log("3500포트에 돌아감 http://localhost:3500")
+  // }
 })
